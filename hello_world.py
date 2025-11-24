22e0082bf7ba126f8250609392b1042ca17b577a
Jani Suvela <jani.suvela@tuni.fi>
2025-11-24 17:55:45 +0200

    hello world in main

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,1 @@
-"""
-COMP.CS.100 Ohjelmointi 1
-Tekijä: Jani Suvela
-Opiskelijanumero: 151946754
-"""
-
-
-def main():
-
-    print("Hello world!")
-
-if __name__ == "__main__":
-    main()
+print("Hello world!")
